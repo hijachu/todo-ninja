57587de9e00967480169fb534b3c06d1cec30c4d
YuChih.Hu <yuchih.hu@zyxel.com.tw>
2023-02-02 14:34:17 +0800

    Tutorial #5 - Buttons & Icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,31 @@
     <p class="subtitle-2 font-weight-bold">This is a subheading</p>
     <p class="caption">This is a caption</p>
 
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed color="pink" dark>click me</v-btn>
+    <v-btn plain color="pink">click me</v-btn>
 
+    <v-btn depressed color="pink" dark>
+      <v-icon left>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+      <v-icon right large>mdi-email</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <br/>
+    <br/>
   </div>
 </template>
 
