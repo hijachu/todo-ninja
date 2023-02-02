b3a8dfd08366e62227b226f247e09770279a9f4f
YuChih.Hu <yuchih.hu@zyxel.com.tw>
2023-02-02 11:41:00 +0800

    tutorial #4 - Text & Colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, aliquid? Numquam facere delectus est illum, id cum beatae modi hic et reiciendis architecto maxime itaque adipisci pariatur. Delectus, consectetur non.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, provident. Velit sunt illum, eum praesentium quibusdam laborum dolorem unde est ad incidunt at aliquam earum dignissimos dolores doloremque! Qui, iusto!</p>
+    <h1>Massive Display</h1>
+    <h1 class="display-4">Massive Display</h1>
+    <h4>Smaller Display</h4>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="text-h1">This is a headline</p>
+    <p class="subtitle-2">This is a subheading</p>
+    <p class="subtitle-2 font-weight-bold">This is a subheading</p>
+    <p class="caption">This is a caption</p>
+
+
   </div>
 </template>
 
