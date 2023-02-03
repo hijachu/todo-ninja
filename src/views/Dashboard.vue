328b58827685f148e825e09d94ef903ef17005e7
YuChih.Hu <yuchih.hu@zyxel.com.tw>
2023-02-03 18:07:58 +0800

    breakpoint props update

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,25 +5,25 @@
       <v-container class="my-5">
 
         <v-row>
-          <v-col xs12 md6>
+          <v-col xs=12 md=6>
             <v-btn tile outlined color="primary" block>1</v-btn>
           </v-col>
-          <v-col xs4 md2>
+          <v-col xs=4 md=2>
             <v-btn tile outlined color="primary" block>2</v-btn>
           </v-col>
-          <v-col xs4 md2>
+          <v-col xs=4 md=2>
             <v-btn tile outlined color="primary" block>2</v-btn>
           </v-col>
-          <v-col xs4 md2>
+          <v-col xs=4 md=2>
             <v-btn tile outlined color="primary" block>2</v-btn>
           </v-col>
         </v-row>
 
-        <v-row justify>
-          <v-col xs4 md3>
+        <v-row justify-end>
+          <v-col xs=4 md=3>
             <v-btn outlined tile color="success" block>1</v-btn>
           </v-col>
-          <v-col xs4 md3>
+          <v-col xs=4 md=3>
             <v-btn outlined tile color="success" block>2</v-btn>
           </v-col>
         </v-row>
