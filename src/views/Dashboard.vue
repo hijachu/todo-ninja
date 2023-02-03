306a1c9c31454960ee46e8049d27a8eed172e849
YuChih.Hu <yuchih.hu@zyxel.com.tw>
2023-02-03 17:57:15 +0800

    Tutorial #13 -The Grid System (part 1)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,31 @@
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
 
       <v-container class="my-5">
-        content
+
+        <v-row>
+          <v-col xs12 md6>
+            <v-btn tile outlined color="primary" block>1</v-btn>
+          </v-col>
+          <v-col xs4 md2>
+            <v-btn tile outlined color="primary" block>2</v-btn>
+          </v-col>
+          <v-col xs4 md2>
+            <v-btn tile outlined color="primary" block>2</v-btn>
+          </v-col>
+          <v-col xs4 md2>
+            <v-btn tile outlined color="primary" block>2</v-btn>
+          </v-col>
+        </v-row>
+
+        <v-row justify>
+          <v-col xs4 md3>
+            <v-btn outlined tile color="success" block>1</v-btn>
+          </v-col>
+          <v-col xs4 md3>
+            <v-btn outlined tile color="success" block>2</v-btn>
+          </v-col>
+        </v-row>
+
       </v-container>
 
   </div>
