e5dafb2b409ba13cb9c971db6370b979a40d4615
YuChih.Hu <yuchih.hu@zyxel.com.tw>
2023-02-06 17:33:03 +0800

    Tutorial #13 - Grid System (part 1)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,43 +2,38 @@
   <div class="dashboard">
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
 
-      <v-container class="my-5">
+    <v-container class="my-5">
+      <v-row no-gutters class="my-3">
+        <v-col sm="12" md="6">
+          <v-btn tile outlined color="primary" block>1</v-btn>
+        </v-col>
+        <v-col sm="4" md="2">
+          <v-btn tile outlined color="primary" block>2</v-btn>
+        </v-col>
+        <v-col sm="4" md="2">
+          <v-btn tile outlined color="primary" block>3</v-btn>
+        </v-col>
+        <v-col sm="4" md="2">
+          <v-btn tile outlined color="primary" block>4</v-btn>
+        </v-col>
+      </v-row>
 
-        <v-row>
-          <v-col xs=12 md=6>
-            <v-btn tile outlined color="primary" block>1</v-btn>
-          </v-col>
-          <v-col xs=4 md=2>
-            <v-btn tile outlined color="primary" block>2</v-btn>
-          </v-col>
-          <v-col xs=4 md=2>
-            <v-btn tile outlined color="primary" block>2</v-btn>
-          </v-col>
-          <v-col xs=4 md=2>
-            <v-btn tile outlined color="primary" block>2</v-btn>
-          </v-col>
-        </v-row>
-
-        <v-row justify-end>
-          <v-col xs=4 md=3>
-            <v-btn outlined tile color="success" block>1</v-btn>
-          </v-col>
-          <v-col xs=4 md=3>
-            <v-btn outlined tile color="success" block>2</v-btn>
-          </v-col>
-        </v-row>
-
-      </v-container>
-
+      <v-row no-gutters justify="space-around">
+        <v-col sm="4" md="3">
+          <v-btn outlined tile color="success" block>1</v-btn>
+        </v-col>
+        <v-col sm="4" md="3">
+          <v-btn outlined tile color="success" block>2</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
+export default {
+  name: "dashboard",
 
-  export default {
-    name: 'dashboard',
-
-    components: {
-    },
-  }
+  components: {},
+};
 </script>
